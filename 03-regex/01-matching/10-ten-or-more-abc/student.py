279e120bd1647895a8e9b9741bd262453ddc039c
XanderDhondtUCLL <xander.dhondt@student.ucll.be>
2024-03-10 23:47:43 +0100

    made 10-13

--- 03-regex/01-matching/10-ten-or-more-abc/student.py
+++ 03-regex/01-matching/10-ten-or-more-abc/student.py
@@ -1,1 +1,5 @@
+import re
+
 # Write your code here
+def ten_or_more_abc(string):
+    return re.fullmatch("(abc){10,}", string)
--- 03-regex/01-matching/12-abc-or-xyz/student.py
+++ 03-regex/01-matching/12-abc-or-xyz/student.py
@@ -1,1 +1,5 @@
+import re
 # Write your code here
+
+def abc_or_xyz(string):
+    return re.fullmatch("(abc)|(xyz)", string)
